1a11e22723954e83482ebe32cc09f8cf0847c10b
Randall Jordan <randallmjordan@comcast.net>
2015-03-06 19:53:17 -0500

    Create BitsPlease.java

---
+++ BitsPlease.java
@@ -0,0 +1,16 @@
+package bitsplease;
+
+public class BitsPlease 
+{
+   public static void main(String[] args)
+   {
+      
+      try
+      {
+         BitsPleaseLogin bpGo = new BitsPleaseLogin();
+      }
+      catch (Exception e)
+      {
+      }
+   }
+}
